b1ebe4f6d4a251d5a2ebbe24cf5b14d8f7b71071
Natan Altomar <89466514+Natanalpe@users.noreply.github.com>
2024-06-05 12:40:28 -0300

    ADD: unform lib & registro de input p/ unform

--- src/pages/pessoas/DetalheDePessoas.tsx
+++ src/pages/pessoas/DetalheDePessoas.tsx
@@ -1,32 +1,74 @@
 import { useNavigate, useParams } from "react-router-dom"
 import { LayoutBaseDePagina } from "../../shared/layouts";
 import { FerramentasDeDetalhe } from "../../shared/components";
+import { useEffect, useState } from "react";
+import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
+import { LinearProgress, TextField } from "@mui/material";
+import { Form } from "@unform/web";
+import { VTextField } from "../../shared/forms";
 
 export const DetalheDePessoas: React.FC = () => {
 
     const navigate = useNavigate();
-
     const { id = 'nova' } = useParams<'id'>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [nome, setNome] = useState('');
+
+    useEffect(() => {
+        if (id !== 'nova') {
+            setIsLoading(true);
+
+            PessoasService.getById(Number(id))
+                .then(result => {
+                    setIsLoading(false);
+
+                    if (result instanceof Error) {
+                        alert(result.message);
+                        navigate('/pessoas');
+                    } else {
+                        setNome(result.nomeCompleto);
+                        console.log(result);
+                    }
+                })
+        }
+    }, [id]);
+
+    const handleDelete = (id: number) => {
+        if (confirm("Deseja realmente apagar?")) {
+          PessoasService.deleteById(id)
+            .then(result => {
+              if (result instanceof Error) {
+                alert(result.message);
+              } else {
+                alert('Pessoa apagada!');
+                navigate('/pessoas');
+              }
+            });
+        }
+      }
 
     return (
         <LayoutBaseDePagina
-            titulo="Detalhe de pessoa"
+            titulo={id === 'nova' ? 'Nova pessoa' : nome}
             barraDeFerramentas={
-                <FerramentasDeDetalhe 
+                <FerramentasDeDetalhe
                     textoBotaoNovo="Nova"
                     mostrarBotaoSalvarEFechar
                     mostrarBotaoNovo={id !== 'nova'}
                     mostrarBotaoApagar={id !== 'nova'}
-                    
-                    aoClicarEmNovo={() => {}}
-                    aoClicarEmSalvar={() => {}}
-                    aoClicarEmApagar={() => {}}
-                    aoClicarEmVoltar={() => navigate('/pessoas')}
+
+                    aoClicarEmSalvar={() => { }}
                     aoClicarEmSalvarEFechar={() => {}}
+                    aoClicarEmVoltar={() => navigate('/pessoas')}
+                    aoClicarEmApagar={() => handleDelete(Number(id))}
+                    aoClicarEmNovo={() => navigate('/pessoas/detalhe/nova')}
                 />
             }
         >
-
+            <Form onSubmit={(data) => console.log(data)}>
+                <VTextField name='nomeCompleto' />
+                <button type='submit'>submit</button>
+            </Form>
         </LayoutBaseDePagina>
     )
 }
---
+++ src/shared/forms/VTextField.tsx
@@ -0,0 +1,37 @@
+import { TextField, TextFieldProps } from "@mui/material";
+import { useField } from "@unform/core";
+import { useEffect, useState } from "react";
+
+type TTextFieldProps = TextFieldProps & {
+    name: string;
+}
+
+export const VTextField: React.FC<TTextFieldProps> = ({ name, ...rest }) => {
+
+    const { fieldName, clearError, defaultValue, error, registerField } = useField(name);
+
+    const [value, setValue] = useState(defaultValue || '');
+
+    useEffect(() => {
+        registerField({
+            name: fieldName,
+            getValue: () => value,
+            setValue: (_, newValue) => setValue(newValue)
+        });
+    }, [registerField, fieldName, value]);
+
+    return (
+        <TextField 
+            error={!!error}
+            helperText={error}
+            defaultValue={defaultValue}
+            
+            {...rest}
+            
+            value={value}
+            onChange={e => setValue(e.target.value)}
+
+            onKeyDown={() => error ? clearError : undefined}
+        />
+    );
+}
