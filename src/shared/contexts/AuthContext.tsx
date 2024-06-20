00eea8bcfacef9716484208fa9a09b67b2e82507
Natan Altomar <89466514+Natanalpe@users.noreply.github.com>
2024-06-20 12:28:20 -0300

    ADD: auth context

---
+++ src/shared/contexts/AuthContext.tsx
@@ -0,0 +1,56 @@
+import React, { createContext, useCallback, useEffect, useMemo, useState } from "react";
+import { AuthService } from "../services/api/auth/AuthService";
+
+
+interface IAuthContextData {
+    logout: () => void;
+    isAuthenticated: boolean;
+    login: (email: string, password: string) => Promise<string | void>;
+}
+
+const AuthContext = createContext({} as IAuthContextData);
+
+interface IAuthProviderProps {
+    children: React.ReactNode;
+}
+
+export const AuthProvider: React.FC<IAuthProviderProps> = ({ children }) => {
+    const LOCALSTORAGE_KEY_ACCESS_TOKEN = 'APP_ACCESS_TOKEN';
+
+    const [accessToken, setAccessToken] = useState<string>();
+
+    useEffect(() => {
+        const accessToken = localStorage.getItem(LOCALSTORAGE_KEY_ACCESS_TOKEN);
+
+        if (accessToken) {
+            setAccessToken(JSON.parse(accessToken));
+        } else {
+            setAccessToken(undefined);
+        }
+    }, []);
+
+    const handleLogin = useCallback(async (email: string, password: string) => {
+        const result = await AuthService.auth(email, password);
+
+        if (result instanceof Error) {
+            return result.message;
+        } else {
+            localStorage.setItem(LOCALSTORAGE_KEY_ACCESS_TOKEN, JSON.stringify(result.accessToken));
+            setAccessToken(result.accessToken);
+        }
+
+    }, []);
+
+    const handleLogout = useCallback(() => {
+        localStorage.removeItem(LOCALSTORAGE_KEY_ACCESS_TOKEN);
+        setAccessToken(undefined);
+    }, []);
+
+    const isAuthenticated = useMemo(() => !!accessToken, [accessToken]);
+
+    return (
+        <AuthContext.Provider value={{ isAuthenticated: isAuthenticated, login: handleLogin, logout: handleLogout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
